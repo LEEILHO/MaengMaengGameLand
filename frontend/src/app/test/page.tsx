f2a828b02ae60162c1cbe596a0d6eb02ce0162e3
ksg2388 <ksg2388@naver.com>
2023-10-24 14:54:22 +0900

    Feat(common): 공통 버튼 구현

--- frontend/src/app/test/page.tsx
+++ frontend/src/app/test/page.tsx
@@ -1,13 +1,29 @@
 'use client'
 
+import CButton from '@components/common/clients/CButton'
 import { styled } from 'styled-components'
 
 const Test = () => {
-  return <StyledTestContainer>Test</StyledTestContainer>
+  return (
+    <StyledTestContainer>
+      <p>Test</p>
+      <CButton
+        radius={36}
+        backgroundColor="rgba(112, 0, 255, 1)"
+        text="테스트 버튼"
+        fontSize={16}
+        color="white"
+      ></CButton>
+    </StyledTestContainer>
+  )
 }
 
 export default Test
 
 const StyledTestContainer = styled.div`
-  font-weight: bold;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
