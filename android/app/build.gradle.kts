512c905732be2cb27658b7b5df77a273e43b2297
jinyoung <kimjinzer099@gmail.com>
2023-10-25 15:02:51 +0900

    chore(android): sdp 라이브러리 추가, 인터넷 권한 설정

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -101,4 +101,7 @@ dependencies {
     implementation ("androidx.room:room-runtime:$room_version")
     kapt ("androidx.room:room-compiler:$room_version")
     implementation ("androidx.room:room-ktx:$room_version")
+
+    //sdp
+    implementation ("com.intuit.sdp:sdp-android:1.1.0")
 }
