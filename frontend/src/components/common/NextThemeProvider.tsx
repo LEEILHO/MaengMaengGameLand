b83a6866c9bbecbdda7329225f283d2ed2a159e5
ksg2388 <ksg2388@naver.com>
2023-10-26 12:35:11 +0900

    Fix(front):  사파리 가로 모드 코드 수정

--- frontend/src/components/common/NextThemeProvider.tsx
+++ frontend/src/components/common/NextThemeProvider.tsx
@@ -6,23 +6,32 @@ import { useEffect } from 'react'
 import { ThemeProvider } from 'styled-components'
 
 const NextThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  let mobile = false
+  let mobile = true
 
   useEffect(() => {
-    function isMobile() {
+    function isIos() {
       if (typeof window !== 'undefined') {
         var user = window.navigator.userAgent
         return (
           user.indexOf('iPhone') > -1 ||
-          user.indexOf('Android') > -1 ||
+          // user.indexOf('Android') > -1 ||
           user.indexOf('iPad') > -1 ||
           user.indexOf('iPod') > -1
         )
       }
       return false
     }
-
-    mobile = isMobile()
+    // 사파리에서 가로모드 설정
+    function handleOrientationChange() {
+      if (window.orientation === 0 || window.orientation === -180) {
+        // 가로 모드에 대한 처리
+        mobile = isIos()
+      }
+    }
+    window.addEventListener('orientationchange', handleOrientationChange)
+    return () => {
+      window.removeEventListener('orientationchange', handleOrientationChange)
+    }
   }, [])
 
   return (
