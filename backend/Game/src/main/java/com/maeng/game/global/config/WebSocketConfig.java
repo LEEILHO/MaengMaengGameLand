2d97b1f41986799981257748a1326417d046ebc8
tlalswn23 <tlalswn23@daum.net>
2023-10-30 16:17:44 +0900

    Fix(back) : rabbitmq config 수정

--- backend/Game/src/main/java/com/maeng/game/global/config/WebSocketConfig.java
+++ backend/Game/src/main/java/com/maeng/game/global/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.maeng.game.global.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.util.AntPathMatcher;
@@ -13,6 +14,12 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer { // STOMP 메세지 처리 방법 구성
 
+    @Value("${spring.rabbitmq.host}")
+    private String HOST;
+    @Value("${spring.rabbitmq.username}")
+    private String USERNAME;
+    @Value("${spring.rabbitmq.password}")
+    private String PASSWORD;
 
     // 메세지를 라우팅하는 메세지 브로커 구성
     @Override
@@ -20,7 +27,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer { // ST
         registry.setPathMatcher(new AntPathMatcher("."));
         //registry.enableSimpleBroker("/queue", "/exchange", "/topic"); // 해당 주소를 구독하는 클라이언트에게 메세지 전송
         registry.setApplicationDestinationPrefixes("/pub"); // 메세지의 목적지 prefix 설정 : /pub으로 시작하는 요청만 메세지 브로커가 받음
-        registry.enableStompBrokerRelay("/queue", "/exchange", "/topic");
+        registry.enableStompBrokerRelay("/queue", "/exchange", "/topic")
+                .setRelayPort(61613)
+                .setRelayHost(HOST)
+                .setSystemLogin(USERNAME)
+                .setSystemPasscode(PASSWORD)
+                .setClientLogin(USERNAME)
+                .setClientPasscode(PASSWORD);
     }
 
     // 메세지를 전송할 endPoint 설정
