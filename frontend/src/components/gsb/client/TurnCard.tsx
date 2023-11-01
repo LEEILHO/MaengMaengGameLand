d1697d7418ab5efa68d115a68c7c1d512603a092
심은진 <seg412@naver.com>
2023-11-01 17:37:24 +0900

    Feat(gsb): 순서 카드 뒤집기 구현

--- frontend/src/components/gsb/client/TurnCard.tsx
+++ frontend/src/components/gsb/client/TurnCard.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from 'react'
 import * as S from '@styles/gsb/OrderingCard.styled'
 import { TurnListType } from '@type/gsb/game.type'
+import { images } from '@constants/images'
 
 // 선후공 카드가 나오는 페이지
 const TurnCard = () => {
@@ -18,12 +19,37 @@ const TurnCard = () => {
     },
   ])
 
-  const onChoiceCard = () => {}
+  const onChoiceCard = (index: number) => {
+    if (cardChoice || orderList[index].selected) return
+    setCardChoice(true)
+    let newOrderList = [...orderList]
+    orderList[index].selected = true
+    setOrderList(newOrderList)
+  }
 
   return (
     <S.OrderingCardContainer>
       <S.DisplayBoard>카드를 뒤집어 선공을 정해주세요.</S.DisplayBoard>
-      <S.CardList></S.CardList>
+      <S.CardList>
+        {orderList.map((order, index) => (
+          <S.CardContainer
+            key={index}
+            initial={false}
+            animate={{ rotateY: order.selected ? 180 : 360 }}
+            transition={{ duration: 0.4, animationDirection: 'normal' }}
+            onClick={() => {
+              onChoiceCard(index)
+            }}
+          >
+            <S.CardFront
+              src={
+                order.first ? images.gsb.cardFrontSun : images.gsb.cardFrontHu
+              }
+            />
+            <S.CardBack src={images.gsb.cardBack} />
+          </S.CardContainer>
+        ))}
+      </S.CardList>
     </S.OrderingCardContainer>
   )
 }
--- frontend/src/styles/gsb/OrderingCard.styled.ts
+++ frontend/src/styles/gsb/OrderingCard.styled.ts
@@ -1,4 +1,5 @@
 import { styled } from 'styled-components'
+import { motion } from 'framer-motion'
 
 export const OrderingCardContainer = styled.div`
   width: 100%;
@@ -32,4 +33,34 @@ export const DisplayBoard = styled.div`
   line-height: normal;
 `
 
-export const CardList = styled.div``
+export const CardList = styled.div`
+  display: flex;
+  gap: 96px;
+`
+
+export const CardContainer = styled(motion.button)`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: none;
+  transition: transform 0.4s;
+  transform-style: preserve-3d;
+  width: 130px;
+  height: 183px;
+`
+
+export const CardFront = styled(motion.img)`
+  position: absolute;
+  backface-visibility: hidden;
+  transform: rotateY(180deg);
+  width: 100%;
+  height: 100%;
+`
+
+export const CardBack = styled(motion.img)`
+  position: absolute;
+  backface-visibility: hidden;
+  width: 100%;
+  height: 100%;
+`
