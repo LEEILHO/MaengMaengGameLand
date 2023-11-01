504ae91ce06f32118f975b04530ea69d36a1b3a1
심은진 <seg412@naver.com>
2023-11-01 10:34:01 +0900

    Feat(gsb): 폴더 생성

---
+++ frontend/src/app/gsb/game-room/[roomId]/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import withAuth from '@components/hoc/client/PrivateRoute'
+import React from 'react'
+
+const GameRoom = () => {
+  return <div>page</div>
+}
+
+export default withAuth(GameRoom)
