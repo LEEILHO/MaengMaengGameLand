88f83e3843d635c4f6d1e2e54389ec2e3860ee64
ksg2388 <ksg2388@naver.com>
2023-11-03 13:09:51 +0900

    Feat(frontend): 모바일 환경에서 스와이프 뒤로가기, 앞으로 가기 막기

--- frontend/src/type/common/common.type.ts
+++ frontend/src/type/common/common.type.ts
@@ -2,4 +2,10 @@ export type UserInformationType = {
   userEmail: string
   nickname: string
   profile: string
+  score: number
+  tier: TireType
+  win: number
+  lose: number
 }
+
+export type TireType = 'BRONZE' | 'SILVER' | 'GOLD' | 'CHALLENGER'
