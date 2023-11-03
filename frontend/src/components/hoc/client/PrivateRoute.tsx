fd8de32c707df962a19f9f295df9ba908fb6327e
ksg2388 <ksg2388@naver.com>
2023-11-03 13:42:30 +0900

    Fix(frontend): 인터셉터 코드에 유저 정보 초기화 코드 추가

--- frontend/src/components/hoc/client/PrivateRoute.tsx
+++ frontend/src/components/hoc/client/PrivateRoute.tsx
@@ -1,15 +1,12 @@
 'use client'
 
 import useAxiosInterceptor from '@hooks/useAxiosInterceptor'
-import useInitUser from '@hooks/useInitUser'
 import { NextPage } from 'next'
 import React from 'react'
 
 const withAuth = (Component: NextPage) => {
-  const initUser = useInitUser()
   const Auth = () => {
     useAxiosInterceptor()
-    initUser()
 
     return <Component />
   }
