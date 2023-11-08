c95739114c215d484d2dde90ef3591fc41527865
ksg2388 <ksg2388@naver.com>
2023-11-08 16:02:28 +0900

    Feat(index): 초기 화면에서 앱 설치 prompt 나오도록 추가

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,28 @@
 'use client'
 
+import useA2HS from '@hooks/useA2HS'
 import { useRouter } from 'next/navigation'
 import { styled } from 'styled-components'
+import { useEffect } from 'react'
 
 export default function Home() {
   const router = useRouter()
+  const { deferredPrompt, installApp, clearPrompt } = useA2HS()
+
+  useEffect(() => {
+    console.log(deferredPrompt)
+  }, [deferredPrompt])
+
   return (
     <>
       <HomeContainer onClick={() => router.replace('/home')}>
         Hello World!
+        {deferredPrompt && (
+          <div>
+            <button onClick={clearPrompt}>취소</button>
+            <button onClick={installApp}>홈 화면에 추가</button>
+          </div>
+        )}
       </HomeContainer>
     </>
   )
