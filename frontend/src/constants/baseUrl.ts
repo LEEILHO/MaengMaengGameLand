db418e75c4be3845513514ca766828fc178f189d
ksg2388 <ksg2388@naver.com>
2023-11-08 12:28:48 +0900

    Fix(frontend): BaseUrl 수정

--- frontend/src/constants/baseUrl.ts
+++ frontend/src/constants/baseUrl.ts
@@ -1,4 +1,4 @@
-export const SOCKET_URL = 'https://maengland.com:9200/maeng'
+export const SOCKET_URL = 'http://maengland.com:9200/maeng'
 export const KAKAO_REDIRECT_URL = 'https://maengland.com/login/kakao'
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URL}&response_type=code`
 export const SERVER_BASE_URL = 'https://maengland.com/api/v1/'
