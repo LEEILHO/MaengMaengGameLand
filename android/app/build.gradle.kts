a4523aede086a8e3e1f1e585e1bbad204c644cb5
jinyoung <kimjinzer099@gmail.com>
2023-11-09 15:58:29 +0900

    Chore(android): Hilt 라이브러리 버전 업그레이드

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id ("org.jetbrains.kotlin.kapt")
-    id ("dagger.hilt.android.plugin")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
+    id("kotlin-parcelize")
 //    id ("androidx.navigation.safeargs.kotlin")
 }
 
@@ -29,11 +30,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     viewBinding {
         enable = true
@@ -62,23 +63,23 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     //retrofit
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-scalars:2.3.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-scalars:2.3.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     // okhttp
     implementation("com.squareup.okhttp3:okhttp:4.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
 
     // ViewModel
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1")
-    implementation ("androidx.activity:activity-ktx:1.7.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1")
+    implementation("androidx.activity:activity-ktx:1.7.2")
 
     // liveData
 //    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.4.1")
 
     // framework ktx
-    implementation ("androidx.fragment:fragment-ktx:1.6.0")
+    implementation("androidx.fragment:fragment-ktx:1.6.0")
 
     // Jetpack Navigation Kotlin
 //    val nav_version = "2.4.2"
@@ -86,25 +87,30 @@ dependencies {
 //    implementation ("androidx.navigation:navigation-ui-ktx:$nav_version")
 
     // material ui
-    implementation ("com.google.android.material:material:1.5.0")
+    implementation("com.google.android.material:material:1.5.0")
 
     // glide
-    implementation ("com.github.bumptech.glide:glide:4.12.0")
-    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
 
     // hilt
-    implementation ("com.google.dagger:hilt-android:2.44")
-    kapt ("com.google.dagger:hilt-compiler:2.44")
+    implementation("com.google.dagger:hilt-android:2.48.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.48.1")
 
     // room
-    val room_version = "2.4.3"
-    implementation ("androidx.room:room-runtime:$room_version")
-    kapt ("androidx.room:room-compiler:$room_version")
-    implementation ("androidx.room:room-ktx:$room_version")
+//    val room_version = "2.4.3"
+//    implementation("androidx.room:room-runtime:$room_version")
+//    kapt("androidx.room:room-compiler:$room_version")
+//    implementation("androidx.room:room-ktx:$room_version")
 
     // sdp
-    implementation ("com.intuit.sdp:sdp-android:1.1.0")
+    implementation("com.intuit.sdp:sdp-android:1.1.0")
 
     // lottie
-    implementation ("com.airbnb.android:lottie:6.1.0")
+    implementation("com.airbnb.android:lottie:6.1.0")
+
+
 }
+//kapt {
+//    correctErrorTypes = true
+//}
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,6 +5,6 @@ plugins {
     //safeArgs
 //    id ("androidx.navigation.safeargs") version "2.4.2" apply false
     //hilt
-    id ("com.google.dagger.hilt.android") version "2.44" apply false
-    id ("com.android.library") version "7.4.1" apply false
+    id("com.google.dagger.hilt.android") version "2.48.1" apply false
+    id("com.android.library") version "7.4.1" apply false
 }
