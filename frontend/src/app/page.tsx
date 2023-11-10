ea56f882ca85567658cacf0908bca292969891eb
ksg2388 <ksg2388@naver.com>
2023-11-10 15:14:01 +0900

    Feat(index): 다운로드 버튼 클릭시 앱 다운되도록 추가

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,9 +8,14 @@ import { useEffect } from 'react'
 import { images } from '@constants/images'
 
 export default function Home() {
-  const router = useRouter()
   const { deferredPrompt, installApp, clearPrompt } = useA2HS()
   const isDownload = deferredPrompt ? true : false
+
+  const handleDownload = () => {
+    if (isDownload) {
+      installApp()
+    }
+  }
 
   useEffect(() => {
     console.log(deferredPrompt)
@@ -23,7 +28,7 @@ export default function Home() {
           <S.TeamName>덜 지니어스</S.TeamName>
           <S.AppName>맹맹게임랜드</S.AppName>
           <S.AppDiscription>꿈과 환상의 나라로 어서오세요!</S.AppDiscription>
-          <S.DownLoadButton $isDownload={isDownload}>
+          <S.DownLoadButton $isDownload={isDownload} onClick={handleDownload}>
             <img
               src={
                 isDownload ? images.index.icon : images.index.donwloadComplete
@@ -34,12 +39,6 @@ export default function Home() {
           </S.DownLoadButton>
         </S.IntroSection>
         <S.GameDiscriptionSection></S.GameDiscriptionSection>
-        {/* {deferredPrompt && (
-          <div>
-            <button onClick={clearPrompt}>취소</button>
-            <button onClick={installApp}>홈 화면에 추가</button>
-          </div>
-        )} */}
       </S.IndexContainer>
     </>
   )
