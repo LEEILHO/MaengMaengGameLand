8a953d19b43a8c2e0c39bb3de7e70e26914c363c
ksg2388 <ksg2388@naver.com>
2023-11-10 15:08:44 +0900

    Feat(Index): 앱 다운로드 페이지 추가

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,16 @@
 'use client'
 
+import * as S from '@styles/index/Index.styled'
 import useA2HS from '@hooks/useA2HS'
 import { useRouter } from 'next/navigation'
 import { styled } from 'styled-components'
 import { useEffect } from 'react'
+import { images } from '@constants/images'
 
 export default function Home() {
   const router = useRouter()
   const { deferredPrompt, installApp, clearPrompt } = useA2HS()
+  const isDownload = deferredPrompt ? true : false
 
   useEffect(() => {
     console.log(deferredPrompt)
@@ -15,19 +18,29 @@ export default function Home() {
 
   return (
     <>
-      <HomeContainer onClick={() => router.replace('/home')}>
-        Hello World!
-        {deferredPrompt && (
+      <S.IndexContainer>
+        <S.IntroSection>
+          <S.TeamName>덜 지니어스</S.TeamName>
+          <S.AppName>맹맹게임랜드</S.AppName>
+          <S.AppDiscription>꿈과 환상의 나라로 어서오세요!</S.AppDiscription>
+          <S.DownLoadButton $isDownload={isDownload}>
+            <img
+              src={
+                isDownload ? images.index.icon : images.index.donwloadComplete
+              }
+              alt="icon"
+            />
+            <p>{deferredPrompt ? '다운로드' : '다운로드 완료'}</p>
+          </S.DownLoadButton>
+        </S.IntroSection>
+        <S.GameDiscriptionSection></S.GameDiscriptionSection>
+        {/* {deferredPrompt && (
           <div>
             <button onClick={clearPrompt}>취소</button>
             <button onClick={installApp}>홈 화면에 추가</button>
           </div>
-        )}
-      </HomeContainer>
+        )} */}
+      </S.IndexContainer>
     </>
   )
 }
-
-const HomeContainer = styled.div`
-  font-size: 80px;
-`
