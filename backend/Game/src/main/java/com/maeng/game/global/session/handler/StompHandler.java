71c49f086c5d6155ade59a66a5af5eaf8b26124c
tlalswn23 <tlalswn23@daum.net>
2023-11-13 14:57:25 +0900

    Fix(back) : 탈주 처리 주석

--- backend/Game/src/main/java/com/maeng/game/global/session/handler/StompHandler.java
+++ backend/Game/src/main/java/com/maeng/game/global/session/handler/StompHandler.java
@@ -19,38 +19,43 @@ import java.util.Objects;
 @RequiredArgsConstructor
 @Configuration
 public class StompHandler implements ChannelInterceptor {
-    private final SessionRepository sessionRepository;
-    private final RoomService roomService;
-
-    @Override
-    public Message<?> preSend(Message<?> message, MessageChannel channel) {
-        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
-
-        if(StompCommand.CONNECT == accessor.getCommand()){
-            String nickname = accessor.getNativeHeader("nickname").toString();
-            nickname = nickname.substring(1, nickname.length()-1);
-
-            log.info("CONNECT");
-            sessionRepository.save(Session.builder()
-                            .sessionId(accessor.getSessionId())
-                            .nickname(nickname)
-                    .build());
-        }
-
-        if(StompCommand.DISCONNECT == accessor.getCommand()){
-           // log.info(accessor.getNativeHeader("nickname").toString());
-            log.info("DISCONNECT");
-            Session session = sessionRepository.findBySessionId(accessor.getSessionId());
-
-            if(session.getRoomCode() != null){ // 대기방에 참여중이라면
-                // TODO : 대기방 퇴장 처리
-                log.info("[연결끊김] "+session.getNickname()+" 대기방 퇴장");
-                roomService.disconnectPlayer(session.getRoomCode(), session.getGameCode(), session.getNickname());
-            }
-
-            sessionRepository.delete(session);
-        }
-
-        return ChannelInterceptor.super.preSend(message, channel);
-    }
+//    private final SessionRepository sessionRepository;
+//    private final RoomService roomService;
+//
+//    @Override
+//    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+//        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+//
+//        if(StompCommand.CONNECT == accessor.getCommand()){
+//            String nickname = accessor.getNativeHeader("nickname").toString();
+//            nickname = nickname.substring(1, nickname.length()-1);
+//
+//            log.info("CONNECT");
+//            sessionRepository.save(Session.builder()
+//                            .sessionId(accessor.getSessionId())
+//                            .nickname(nickname)
+//                    .build());
+//        }
+//
+//        if(StompCommand.DISCONNECT == accessor.getCommand()){
+//           // log.info(accessor.getNativeHeader("nickname").toString());
+//            log.info("DISCONNECT");
+//            Session session = sessionRepository.findBySessionId(accessor.getSessionId());
+//
+//            if(session.getRoomCode() != null){ // 대기방에 참여중이라면
+//                // TODO : 대기방 퇴장 처리
+//                log.info("[연결끊김] "+session.getNickname()+" 대기방 퇴장");
+//                roomService.exitRoom(session.getRoomCode(), PlayerDTO.builder().nickname(session.getNickname()).build());
+//            }
+//
+//            if(session.getGameCode() != null){ // 게임 코드만 있을 것
+//                log.info("[연결끊김] "+session.getNickname()+" 게임 중 퇴장");
+//                roomService.disconnectPlayer(session.getRoomCode(), session.getGameCode(), session.getNickname());
+//            }
+//
+//            sessionRepository.delete(session);
+//        }
+//
+//        return ChannelInterceptor.super.preSend(message, channel);
+//    }
 }
