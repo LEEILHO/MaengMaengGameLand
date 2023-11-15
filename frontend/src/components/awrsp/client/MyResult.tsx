6da8e63f63d2e79297863b0f48f7d46b8f869f08
심은진 <seg412@naver.com>
2023-11-15 13:50:49 +0900

    Fix(awrsp): 정답 맞추면 멈추라고..

--- frontend/src/components/awrsp/client/MyResult.tsx
+++ frontend/src/components/awrsp/client/MyResult.tsx
@@ -21,6 +21,7 @@ const MyResult = () => {
   const user = useRecoilValue(userState)
   const [history, setHistory] = useRecoilState(HistoryState)
   const round = useRecoilValue(RoundState)
+  const setStep = useSetRecoilState(StepState)
   const [myResult, setMyResult] = useState<PlayerResultType>()
 
   useEffect(() => {
@@ -39,6 +40,13 @@ const MyResult = () => {
         return
       }
     })
+
+    return () => {
+      if (myResult && myResult.finish) {
+        setStep('WAITING')
+        return
+      }
+    }
   }, [])
 
   if (!myResult) return null
