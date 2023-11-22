c1348a1ab3faf2483d26a4a59d69d7c7b39f317c
tlalswn23 <tlalswn23@daum.net>
2023-11-22 14:31:50 +0900

    Fix(back) : 게임 결과에 정답 카드 추가

--- backend/Game/src/main/java/com/maeng/game/domain/awrsp/dto/RankResultDTO.java
+++ backend/Game/src/main/java/com/maeng/game/domain/awrsp/dto/RankResultDTO.java
@@ -1,18 +1,16 @@
 package com.maeng.game.domain.awrsp.dto;
 
-import com.maeng.game.domain.awrsp.entity.Card;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class GameResultDTO {
-    private List<RankResultDTO> result;
-    private Card[] answer;
+public class RankResultDTO {
+    private String nickname;
+    private int round;
+    private int rank;
 }
